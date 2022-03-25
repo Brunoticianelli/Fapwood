861b36795decd57852591b57d0615d884a1b1a41
Brunoticianelli <bruno.ticianelli@outlook.com>
2022-03-25 14:37:22 -0300

    first commit

---
+++ overviewerConfig.js
@@ -0,0 +1,183 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Fapwood 2022",
+        "Fapwood 2020",
+        "Fapwood 2015",
+        "Fapwood 2013",
+        "Fapwood 2011"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1648224665",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "Fapwood 2022",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2022",
+            "last_rendertime": 1647652078,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                112,
+                -6
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -1,
+                112,
+                -6
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "Fapwood 2020",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2020",
+            "last_rendertime": 1585413320,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -208,
+                80,
+                172
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -208,
+                80,
+                172
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "Fapwood 2015",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2015",
+            "last_rendertime": 1558198158,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                65,
+                -1
+            ],
+            "minZoom": 0,
+            "spawn": [
+                0,
+                65,
+                -1
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "Fapwood 2013",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2013",
+            "last_rendertime": 1447526796,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                64,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": [
+                0,
+                64,
+                0
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Daytime Render",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "Fapwood 2011",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2011",
+            "last_rendertime": 1447522123,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                261,
+                64,
+                227
+            ],
+            "minZoom": 0,
+            "spawn": [
+                261,
+                64,
+                227
+            ],
+            "north_direction": 2
+        }
+    ]
+};
