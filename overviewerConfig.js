b8bb3cca7d190ed4e0c96b8cec6307c3f9f53dd6
Brunoticianelli <bruno.ticianelli@outlook.com>
2025-01-07 14:26:17 -0300

    Fapwood v8

---
+++ overviewerConfig.js
@@ -0,0 +1,167 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Fapwood 2024",
+        "Fapwood 2022",
+        "Fapwood 2015",
+        "Fapwood 2013"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1736214783",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "2024 - Daytime Lado Um",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "Fapwood 2024 - Lado Um Dia",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2024",
+            "last_rendertime": 1735830967,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                112,
+                -6
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "2024 - Daytime Lado Dois",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "Fapwood 2024 - Lado Dois Dia",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2024",
+            "last_rendertime": 1735830967,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                112,
+                -6
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 3
+        },
+        {
+            "name": "2022 - Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "Fapwood 2022 - Lado Um",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2022",
+            "last_rendertime": 1735829897,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                112,
+                -6
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "2015 - Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "Fapwood 2015 - Lado Um",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2015",
+            "last_rendertime": 1735827584,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                128,
+                -1
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "2013 - Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "Fapwood 2013 - Lado Um",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Fapwood 2013",
+            "last_rendertime": 1735820880,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                128,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        }
+    ]
+};
